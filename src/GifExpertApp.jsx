be01675ba0d2724b102dc97e1f2c07e59ecb791b
Jhonnatan Machuca <jmachuca@tuxpan.com>
2025-05-12 16:01:42 -0400

    Comunicación entre componentes

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,12 +1,27 @@
-import { useState } from 'react'
+import { useState } from 'react';
+import { AddCategory } from './components/AddCategory'
 
 export const GifExpertApp = () => {
 
-  const [categories, setCategories] = useState([ 'One Punch', 'Dragon Ball' ]);
+  const [categories, setCategories] = useState([]);
+
+  const onAddCategory = ( category ) => {
+
+    // Agrega al final
+    // setCategories( cat => [ ...cat, 'Naruto' ] );
+    
+    //Agrega al principio
+    setCategories( cat => [ category, ...cat ] );
+
+  }
 
   return (
     <>
+        {/* Titulo */}
         <h1>GifExpertApp</h1>
+
+        {/* Input */}
+        <AddCategory setCategories={ setCategories } />
 
         {/* Listado de Gif */}
         <ol>
