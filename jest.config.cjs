011b8a2e03da31d0c25ca5f81d093716c2f173da
Jhonnatan Machuca <jmachuca@tuxpan.com>
2025-06-03 17:53:19 -0400

    Correcciones configuración pruebas

---
+++ jest.config.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+    testEnvironment: 'jest-environment-jsdom',
+    setupFiles: ['./jest.setup.js'],
+    transform: {
+        '^.+\\.(js|jsx)$': 'babel-jest',
+        '^.+\\.(ts|tsx)$': 'ts-jest',
+    },
+}
